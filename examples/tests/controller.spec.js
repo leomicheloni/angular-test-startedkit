f31b2c92dc716dcf961b7785148a671f933e5382
Leonardo <leomicheloni@hotmail.com>
2016-02-15 22:45:46 -0300

    added initial scenarios and samples

---
+++ examples/tests/controller.spec.js
@@ -0,0 +1,17 @@
+describe("Scenario1", function(){
+	
+	beforeEach(function(){
+		
+	});
+	
+	it("should do something", function(){
+		
+	});
+
+	
+	describe("Sub scenario 1", function(){
+		
+	
+	});
+	
+});
