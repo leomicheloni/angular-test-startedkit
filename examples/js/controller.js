3a18f4e1cd966478177ca5c58b37594ab5dc00ab
Leonardo Micheloni <leomicheloni@hotmail.com>
2016-02-25 13:51:45 -0300

    Update controller.js

--- examples/js/controller.js
+++ examples/js/controller.js
@@ -1,3 +1,4 @@
+// a controller with scope, property and a function that changes the property
 app.controller("mainController", function($scope){
 	$scope.value1 = "i'm a value";
 	$scope.doSomething = function(){
@@ -5,10 +6,12 @@ app.controller("mainController", function($scope){
 	}
 });
 
+// a simple controller with no scope
 app.controller("mainControllerWithoutScope", function(){
 	
 });
 
+// a test service
 app.factory("service1", function(){
 	
 	return {
